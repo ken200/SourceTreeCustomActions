75345399a2459ea84b377c9027258a0ed2cafce4
ken200 <tisp1982@gmail.com>
2014-08-17 07:45:55 +0900

    受け取る引数並びの不具合修正

--- git_checkout.ps1
+++ git_checkout.ps1
@@ -8,8 +8,8 @@ $env:Path += ";C:\Program Files (x86)\Git\bin";
 
 Set-Location $args[0]
 
-$commitHash = $args[$args.Length-1]
-for($i=1;$i -lt $args.Length-1; $i++){
+$commitHash = $args[1]
+for($i=2;$i -lt $args.Length; $i++){
     $filePath = $args[$i]
     git checkout $commitHash $filePath
     git reset $filePath #ステージング解除
