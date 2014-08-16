a6310f0c4393835641a591bddc7580fb5f5c7678
ken200 <tisp1982@gmail.com>
2014-08-16 17:03:54 +0900

    受け取るパラメーターの順番を変更
    以下のように変更した。
    1.リポジトリパス、 2.ハッシュ値、 3～.チェックアウトするファイルパス

--- git_checkout.ps1
+++ git_checkout.ps1
@@ -1,4 +1,10 @@
-﻿$env:Path += ";C:\Program Files (x86)\Git\bin";
+﻿#パラメーターは以下の順序で渡す
+#
+# 1.   リポジトリパス
+# 2.   コミットハッシュ値
+# 3～. チェックアウト対象ファイルパス(リポジトリルートからの絶対パス) 複数可
+
+$env:Path += ";C:\Program Files (x86)\Git\bin";
 
 Set-Location $args[0]
 
