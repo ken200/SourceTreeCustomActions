dcb0667d3d3414fb3d692a5fb8e6d06a03530523
ken200 <tisp1982@gmail.com>
2014-08-25 20:11:47 +0900

    選択した登録すみファイルを管理対象から除外するスクリプトの追加

---
+++ git_rm_cached.ps1
@@ -0,0 +1,16 @@
+﻿#パラメーターは以下の順序で渡す
+# 
+# 1.   コミットハッシュ値(★未使用)
+# 2.   リポジトリパス
+# 3～. チェックアウト対象ファイルパス(リポジトリルートからの絶対パス) 複数可
+
+$env:Path += ";C:\Program Files (x86)\Git\bin";
+
+Set-Location $args[1]
+
+for($i=2;$i -lt $args.Length; $i++){
+    $filePath = $args[$i]
+    git rm --cached $filePath
+}
+
+git commit -m "管理対象から除外"
